8cd0db0e176dd191ee87ab943fc6be5566ef6029
Artem Bulakh <artem.bulakh1@gmail.com>
2021-06-04 02:02:42 +0300

    CourseDetail now is a RetrieveUpdateDestroyAPIView

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,29 +1,36 @@
 from rest_framework import serializers
 
 from courses.models import Course
+from users.models import User
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    teachers = serializers.SlugRelatedField(slug_field='username', read_only=True, many=True)
-    students = serializers.SlugRelatedField(slug_field='username', read_only=True, many=True)
+    teachers = serializers.PrimaryKeyRelatedField(read_only=False, many=True, queryset=User.objects.all())
+    students = serializers.PrimaryKeyRelatedField(read_only=False, many=True, queryset=User.objects.all())
 
     class Meta:
         model = Course
         fields = '__all__'
 
     def create(self, validated_data):
+        print(validated_data)
         course = Course(
-            teacher=validated_data['teacher'],
-            name=validated_data['name'],
+            name=validated_data.get('name'),
+            teacher=validated_data.get('teacher'),
         )
         course.save()
 
+        course.teachers.set(validated_data.get('teachers', []))
         course.teachers.add(validated_data['teacher'])
+        course.students.set(validated_data.get('students', []))
         course.save()
 
         return course
 
     def update(self, instance, validated_data):
-        instance.name = validated_data['name']
+        instance.name = validated_data.get('name', instance.name)
+        instance.teacher = validated_data.get('teacher', instance.teacher)
+        instance.teachers.set(validated_data.get('teachers', instance.teachers.all()))
+        instance.students.set(validated_data.get('students', instance.students.all()))
         instance.save()
         return instance
--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,4 @@ from . import views
 urlpatterns = [
     path('courses/', views.CourseList.as_view()),
     path('courses/<int:pk>/', views.CourseDetail.as_view()),
-
-    path('courses/<int:course_pk>/add_user/<int:user_pk>/', views.CourseUsers.as_view()),
 ]
